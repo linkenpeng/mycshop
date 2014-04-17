400bcf6a0ecbb11b82fa51d2b03c79d2395b6c4a
linkenpeng <linkenpeng@gmail.com>
2014-04-17 10:02:47 +0800

    update config

--- configs/config.db.php
+++ configs/config.db.php
@@ -4,9 +4,9 @@ if (!defined('SYS_IN')) {
 }
 $db_config = array();
 $db_config['db_host'] = "localhost";
-$db_config['db_name'] = "hbrt";
-$db_config['db_user'] = "hbrt";
-$db_config['db_password'] = "hbrt123ewq";
+$db_config['db_name'] = "mycshop";
+$db_config['db_user'] = "mycshop";
+$db_config['db_password'] = "mycshop123ewq";
 $db_config['db_charset'] = "utf8";
 $db_config['db_pconnect'] = 0;
 $db_config['db_table_pre'] = "hb_";
