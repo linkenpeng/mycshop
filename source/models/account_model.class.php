1ff2820531381594fceacf77267fa4777de4aa11
linkenpeng <linkenpeng@gmail.com>
2014-12-19 18:24:58 +0800

    update models.

--- source/models/account_model.class.php
+++ source/models/account_model.class.php
@@ -2,98 +2,11 @@
 defined('SYS_IN') or exit('Access Denied.');
 Base::load_sys_class('model');
 class account_model extends model {
-    private $table = "account";
+    protected $_table = 'account';
+    protected $_primarykey = 'accountid';
+    
     function __construct() {
         parent::__construct();
-    }
-    /**
-     * 
-     * 获取一条信息
-     * @param int $accountid
-     */
-    function get_one($accountid = "") {
-		if(!empty($accountid)) {
-			$where = "where accountid=".$accountid;
-			$sql = "select * from ".tname($this->table)." $where limit 0,1";
-			$value = $this->db->get_one($sql);
-			return $value;
-		} else {
-			return '';
-		}
-    }
-    /**
-     * 
-     * 插入一条信息
-     * @param array $data
-     * return Boolean $flag
-     */
-    function insert($data) {
-        $flag = false;
-        if ($this->db->insert(tname($this->table),$data)) {
-            $flag = true;
-        }
-        return $flag;
-    }
-    /**
-     * 修改一条信息
-     * 
-     * @param array $data
-     * @param string $where
-     * @author Myron
-     * 2011-5-27 上午10:18:10
-     */
-    function update($data, $where) {
-        $flag = false;
-        if (!empty($data)&&!empty($where)) {
-            $this->db->update(tname($this->table),$data,$where);
-            $flag = true;
-        }
-        return $flag;
-    }
-    /**
-     * 
-     * 删除一条信息
-     * @param array $data
-     * return Boolean $flag
-     */
-    function delete($accountid) {
-        $flag = false;
-        $sql = "DELETE FROM ".tname($this->table)." WHERE accountid=".$accountid;
-        if ($this->db->query($sql)) {
-            $flag = true;
-        }
-        return $flag;
-    }
-    /**
-     * 获取一组信息
-     * @param int $num
-     * @param int $offset
-     * @param string $field
-     * @param stirng $where
-     * @param string $accountby
-     * reunt @param array $list
-     */
-    function get_list($num = 10, $offset, $field = '', $where = '', $oderbye = '') {
-        $num = empty($num) ? 10 : intval($num);
-        $offset = (empty($offset)||$offset<0) ? 0 : intval($offset);
-        $field = empty($field) ? ' * ' : $field;
-        $where = empty($where) ? ' WHERE 1 ' : $where;
-        $oderbye = empty($oderbye) ? ' ORDER BY dateline DESC ' : ' ORDER BY '.$oderbye;
-        $sql = "SELECT ".$field." FROM ".tname($this->table).$where.$oderbye." LIMIT $offset,$num ";
-        //echo $sql;
-        $list = $this->db->get_list($sql);
-        return $list;
-    }
-    /**
-     * 获取总数
-     * @param stirng $where
-     * return @param int $count
-     */
-    function get_count($where = '') {
-        $where = empty($where) ? ' WHERE 1 ' : $where;
-        $sql = "SELECT COUNT(*) as c FROM ".tname($this->table).$where;
-        $value = $this->db->get_one($sql);
-        return $value['c'];
-    }
+    }    
 }
 ?>
--- source/utils/class/model.class.php
+++ source/utils/class/model.class.php
@@ -7,9 +7,100 @@ defined('SYS_IN') or exit('Access Denied.');
 class model {
     //数据库连接
     protected $db = '';
+    protected $_table = '';
+    protected $_primarykey = '';
     
     public function __construct() {
         global $db;
         $this->db = $db;
+    }
+    
+    /**
+     *
+     * 获取一条信息
+     * @param int $accountid
+     */
+    function get_one($primarykey = "") {
+    	if(!empty($primarykey)) {
+    		$where = "where ".$this->_primarykey."=".$primarykey;
+    		$sql = "select * from ".tname($this->_table)." $where limit 1";
+    		$value = $this->db->get_one($sql);
+    		return $value;
+    	} else {
+    		return '';
+    	}
+    }
+    /**
+     *
+     * 插入一条信息
+     * @param array $data
+     * return Boolean $flag
+     */
+    function insert($data) {
+    	$flag = false;
+    	if ($this->db->insert(tname($this->_table),$data)) {
+    		$flag = true;
+    	}
+    	return $flag;
+    }
+    /**
+     * 修改一条信息
+     *
+     * @param array $data
+     * @param string $where
+     * @author Myron
+     * 2011-5-27 上午10:18:10
+     */
+    function update($data, $where) {
+    	$flag = false;
+    	if (!empty($data)&&!empty($where)) {
+    		$this->db->update(tname($this->_table),$data,$where);
+    		$flag = true;
+    	}
+    	return $flag;
+    }
+    /**
+     *
+     * 删除一条信息
+     * @param array $data
+     * return Boolean $flag
+     */
+    function delete($primarykey) {
+    	$flag = false;
+    	$sql = "DELETE FROM ".tname($this->_table)." WHERE ".$this->_primarykey."=".$primarykey;
+    	if ($this->db->query($sql)) {
+    		$flag = true;
+    	}
+    	return $flag;
+    }
+    /**
+     * 获取一组信息
+     * @param int $num
+     * @param int $offset
+     * @param string $field
+     * @param stirng $where
+     * @param string $accountby
+     * reunt @param array $list
+     */
+    function get_list($num = 10, $offset, $field = '', $where = '', $oderbye = '') {
+    	$num = empty($num) ? 10 : intval($num);
+    	$offset = (empty($offset)||$offset<0) ? 0 : intval($offset);
+    	$field = empty($field) ? ' * ' : $field;
+    	$where = empty($where) ? ' WHERE 1 ' : $where;
+    	$oderbye = empty($oderbye) ? ' ORDER BY dateline DESC ' : ' ORDER BY '.$oderbye;
+    	$sql = "SELECT ".$field." FROM ".tname($this->_table).$where.$oderbye." LIMIT $offset,$num ";
+    	$list = $this->db->get_list($sql);
+    	return $list;
+    }
+    /**
+     * 获取总数
+     * @param stirng $where
+     * return @param int $count
+     */
+    function get_count($where = '') {
+    	$where = empty($where) ? ' WHERE 1 ' : $where;
+    	$sql = "SELECT COUNT(*) as c FROM ".tname($this->_table).$where;
+    	$value = $this->db->get_one($sql);
+    	return $value['c'];
     }
 }
