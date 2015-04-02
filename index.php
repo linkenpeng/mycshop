7fdb075edf8ff0f821690d9c0cec3c054016b311
linkenpeng <linkenpeng@gmail.com>
2015-04-02 19:20:06 +0800

    update

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 /**
  * index.php 系统 入口
  */
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
 define('ROOT_PATH',str_replace("\\","/",dirname(__FILE__)));
+
 $autoload_paths['app'] = ROOT_PATH.'/source';
 include ROOT_PATH.'/framework/index.php';
 include ROOT_PATH.'/source/base.php';
+
 new trig_mvc_application();
