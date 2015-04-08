b34667a43c32d9f4d75d75ceaaf6aaa08f2bccf0
linkenpeng <linkenpeng@gmail.com>
2015-04-08 14:31:42 +0800

    增加错误处理

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
 /**
  * index.php 系统 入口
  */
-define('DEBUG',true);
-DEBUG && error_reporting(E_ALL & ~E_NOTICE);// & ~E_STRICT & ~E_DEPRECATED
-
-define('ROOT_PATH',str_replace("\\","/",dirname(__FILE__)));
-
-$autoload_paths['app'] = ROOT_PATH.'/source';
-include ROOT_PATH.'/framework/index.php';
-
-include ROOT_PATH.'/source/base.php';
-
+define('DEBUG', true);
+DEBUG && error_reporting(E_ALL & ~E_NOTICE); // & ~E_STRICT & ~E_DEPRECATED
+define('ROOT_PATH', str_replace("\\", "/", dirname(__FILE__)));
+$autoload_paths['app'] = ROOT_PATH . '/source';
+include ROOT_PATH . '/framework/index.php';
+include ROOT_PATH . '/source/base.php';
 new trig_mvc_application();
+
+set_exception_handler('exceptionCallback');
+function exceptionCallback($e) {
+	$ret = array(
+		'code' => $e->getCode(),
+		'msg' => $e->getMessage() 
+	);
+	if (!($e instanceof trig_exception_driver)) {
+		$ret['code'] = 12306;
+		$ret['msg'] = '系统异常';
+	}
+	echo json_encode($ret);
+	exit(0);
+}
--- source/application/base.php
+++ source/application/base.php
@@ -1,6 +1,7 @@
 <?php 
 class application_base extends trig_mvc_controller {
 	function __construct() {
+		// 是否登录
 		$login_model = new model_login();
 		$login_model->is_login();
 	}
